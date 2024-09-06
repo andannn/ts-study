034234d6d5a6254d63061c3d555694b666e0c92c
andannn <jqn296763005@gmail.com>
2024-09-06 20:13:48 +0900

    init

---
+++ Lesson1/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let userName112 = 'Jiangqna';
+console.log(userName112);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
---
+++ Lesson1/src/main.ts
@@ -0,0 +1,12 @@
+
+let userName112 = 'Jiangqna'
+
+console.log(userName112)
+
+let a = 12
+let b = 6
+let c = 2
+
+console.log(a / b)
+
+console.log(c * b)
